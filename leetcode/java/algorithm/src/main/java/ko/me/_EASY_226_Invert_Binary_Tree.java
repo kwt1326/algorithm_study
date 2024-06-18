f69970b49ffe36d76dbb5dc2291997887323a150
김원태 <gim-wontae@gim-wontaeui-MacBookAir.local>
2024-06-19 08:31:23 +0900

    feat: TreeNode implement, solved leetcode/java 226

---
+++ leetcode/java/algorithm/src/main/java/ko/me/_EASY_226_Invert_Binary_Tree.java
@@ -0,0 +1,31 @@
+package ko.me;
+
+import ko.me.dataStructure.TreeNode;
+
+// Recursive
+// https://leetcode.com/problems/invert-binary-tree/
+public class _EASY_226_Invert_Binary_Tree {
+
+    public static void invert(TreeNode node) {
+        if (node == null) return;
+
+        TreeNode temp;
+        temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+
+        invert(node.left);
+        invert(node.right);
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+        invert(root);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {4,2,7,1,3,6,9};
+        TreeNode root = TreeNode.sortedArrayToBST(array);
+        invertTree(root);
+    }
+}
---
+++ leetcode/java/algorithm/src/main/java/ko/me/dataStructure/TreeNode.java
@@ -0,0 +1,82 @@
+package ko.me.dataStructure;
+
+public class TreeNode {
+    public enum TraversalType {
+        PRE,
+        IN,
+        POST
+    }
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(final int val) {
+        this.val = val;
+    }
+
+    TreeNode(
+            final int val,
+            final TreeNode left,
+            final TreeNode right
+    ) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public void print(final TraversalType order) {
+        if (order == TraversalType.PRE) {
+            System.out.print(this.val + " ");
+        }
+        printInorderTraversal(this.left, order);
+        if (order == TraversalType.IN) {
+            System.out.print(this.val + " ");
+        }
+        printInorderTraversal(this.right, order);
+        if (order == TraversalType.POST) {
+            System.out.print(this.val + " ");
+        }
+    }
+
+    // 배열을 BST로 변환하는 메서드
+    public static TreeNode sortedArrayToBST(final int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        return insertLevelOrder(nums, 0);
+    }
+
+    // 트리를 중위 순회하며 출력하는 메서드 (테스트용)
+    public static void printInorderTraversal(final TreeNode root, final TraversalType order) {
+        if (root != null) {
+            if (order == TraversalType.PRE) {
+                System.out.print(root.val + " ");
+            }
+            printInorderTraversal(root.left, order);
+            if (order == TraversalType.IN) {
+                System.out.print(root.val + " ");
+            }
+            printInorderTraversal(root.right, order);
+            if (order == TraversalType.POST) {
+                System.out.print(root.val + " ");
+            }
+        }
+    }
+
+    //region Private Methods
+    // 재귀적으로 노드를 삽입하는 메서드
+    private static TreeNode insertLevelOrder(final int[] arr, final int i) {
+        TreeNode root = null;
+        if (i < arr.length) {
+            root = new TreeNode(arr[i]);
+            root.left = insertLevelOrder(arr, 2 * i + 1);
+            root.right = insertLevelOrder(arr, 2 * i + 2);
+        }
+        return root;
+    }
+    //endregion
+}
